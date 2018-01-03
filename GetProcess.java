7f5b510623ac2a6376dbe5cd98c93260f4c5e80c
sakura-fly <sourcesakura@sina.com>
2018-01-03 14:49:40 +0800

    执行bash

---
+++ GetProcess.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class GetProcess {
+
+public static String executeCommand(String[] cmd){
+    Process proc = null;
+    StringBuffer sb = new StringBuffer();
+    try {
+        proc = Runtime.getRuntime().exec(cmd);
+        InputStream is = proc.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line= "";
+        try {
+        	while ((line = br.readLine()) != null){
+        		sb.append(line);
+        	}
+        	br.close();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }finally{
+        if (proc != null){
+            proc.destroy();
+        }
+    }
+    return sb.toString();
+}
+
+public static void main(String[] args) {
+    String cmd = "ls > lsResult";
+
+    executeCommand(new String[]{"sh", "-c", cmd});
+
+    }
+
+}
